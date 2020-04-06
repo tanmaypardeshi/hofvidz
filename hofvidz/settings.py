be525ba79e6a3fc395e38e40dee382776c35b198
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-04-06 20:30:05 +0530

    Another try

--- hofvidz/settings.py
+++ hofvidz/settings.py
@@ -87,4 +87,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifesrStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
